c661527359978d18f811fc4220b0e9e67618fb37
220100 <220100kd@yse-c.net>
2023-05-25 10:36:49 +0900

    入力時のエラー判定

--- chenge_ch/text_edit/forms.py
+++ chenge_ch/text_edit/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 class TextForm(forms.Form):
     text = forms.CharField()
     search = forms.CharField()
     replace = forms.CharField()
+
+    def clean(self):
+        data = super().clean()  # views.pyのdata(= form.cleaned_data)を呼び出す
+        text = data["text"]
+        # textが5文字以下の時にエラーを発生させる
+        if len(text) <= 5:
+            raise ValidationError("テキストが短すぎます。6文字以上入力してください。")
+        return data
