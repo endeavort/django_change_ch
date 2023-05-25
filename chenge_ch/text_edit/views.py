bb8910bbd4cad0a2d5c8beb569edc62e090ca20c
220100 <220100kd@yse-c.net>
2023-05-25 10:32:59 +0900

    文字変換作成

--- chenge_ch/text_edit/views.py
+++ chenge_ch/text_edit/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.views.generic.edit import FormView
 
 from . import forms
@@ -6,3 +7,17 @@ from . import forms
 class Index(FormView):
     form_class = forms.TextForm
     template_name = "index.html"
+
+    # 入力されたデータに問題がない場合呼ばれる関数
+    def form_valid(self, form) -> HttpResponse:
+        data = form.cleaned_data  # 入力されたデータ(辞書型)
+        text = data["text"]
+        search = data["search"]
+        replace = data["replace"]
+
+        # textの中のsearchをreplaceに変換
+        new_text = text.replace(search, replace)
+
+        # テンプレートに渡す
+        ctxt = self.get_context_data(new_text=new_text, form=form)
+        return self.render_to_response(ctxt)
