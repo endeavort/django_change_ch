ea5d22ae8cd3135b6008bcb48495eddfea5736d1
220100 <220100kd@yse-c.net>
2023-05-25 11:24:20 +0900

    ・2重エラー表示削除 プロジェクト名chengeのスペル訂正

--- change_ch/change_ch/settings.py
+++ change_ch/change_ch/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "text_edit"
+    "text_edit",
 ]
 
 MIDDLEWARE = [
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "chenge_ch.urls"
+ROOT_URLCONF = "change_ch.urls"
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "chenge_ch.wsgi.application"
+WSGI_APPLICATION = "change_ch.wsgi.application"
 
 
 # Database
--- change_ch/text_edit/forms.py
+++ change_ch/text_edit/forms.py
@@ -2,10 +2,18 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 
+widget_textarea = forms.Textarea(
+    # cssを適用させる
+    attrs={"class": "form-control"}
+)
+
+widget_textinput = forms.TextInput(attrs={"class": "form-control"})
+
+
 class TextForm(forms.Form):
-    text = forms.CharField()
-    search = forms.CharField()
-    replace = forms.CharField()
+    text = forms.CharField(label="", widget=widget_textarea)
+    search = forms.CharField(label="検索", widget=widget_textinput)
+    replace = forms.CharField(label="置換", widget=widget_textinput)
 
     def clean(self):
         data = super().clean()  # views.pyのdata(= form.cleaned_data)を呼び出す
